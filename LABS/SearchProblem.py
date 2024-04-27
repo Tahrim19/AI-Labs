1cd2aea8bd1eb460c35af7bad59d16240d2ee929
Tahrim Bilal <141277912+Tahrim19@users.noreply.github.com>
2024-04-27 05:38:46 -0700

    Add files via upload

---
+++ LABS/SearchProblem.py
@@ -0,0 +1,17 @@
+from abc import abstractmethod
+
+class SearchProblem(object):
+    @abstractmethod
+    def __init__(self, params): pass
+
+    @abstractmethod
+    def initialState(self): pass
+
+    @abstractmethod
+    def succesorFunction(self, currentState): pass
+
+    @abstractmethod
+    def isGoal(self, currentState): pass
+
+    @abstractmethod
+    def __str__(self): pass
